c1443604782134cf46732fcc663834654adcf87b
muhammad sahad <muhammadsahad2022@gmail.com>
2024-03-16 14:00:33 +0530

    New DS file

---
+++ DS.js
@@ -0,0 +1,15 @@
+// prime number 
+
+//  function prime(n){
+//   if(n < 2){
+//     return false
+//   }
+//   for(let i = 2; i < n ; i ++){
+//     if(n%i ==0){
+//       return false
+//     }
+//   }
+//   return true
+//  }
+//  console.log(prime(5));
+//  console.log(prime(4)); console.log(prime(1));
