e66811a606c5b22455f8eca30ab1daa6cf960b95
muhammad sahad <muhammadsahad2022@gmail.com>
2024-03-16 14:02:52 +0530

    add febinacc find

--- DS.js
+++ DS.js
@@ -13,3 +13,17 @@
 //  }
 //  console.log(prime(5));
 //  console.log(prime(4)); console.log(prime(1));
+
+// ==================================================================
+
+// febinacci number
+
+// function febbi(n){
+//   let febbi = [0,1]
+//   for( let i = 2;i < n ;i ++){
+//       febbi[i] = febbi[i-1] + febbi[i - 2]
+//   }
+//   return febbi
+// }
+// console.log(febbi(3))
+// console.log(febbi(7))
